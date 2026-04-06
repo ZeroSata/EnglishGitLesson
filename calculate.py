063b6c982f33de49b50ee34eea9ce78844cc5a32
Michael <zerostki0912@gmail.com>
2026-04-06 08:48:01 +0300

    calculate func puthon

---
+++ calculate.py
@@ -0,0 +1,19 @@
+def calculate_sum(addend_1:float, addend_2:float) -> float:
+    sum = addend_1 + addend_2
+    return sum
+
+def calculate_subtraction(minuend:float, subtrahend:float) -> float:
+    subtraction = minuend - subtrahend
+    return subtraction
+
+def calculate_multiplication(fador:float, fador_1:float) -> float:
+    produot = fador * fador_1
+    return produot
+
+def calculate_dirision(dirisor:float, diriend:float) -> float:
+    quotient =  diriend / dirisor 
+    return quotient
+
+def calculate_exporentiotion(base:float, power:float) -> float:
+    result =  power / power 
+    return result
